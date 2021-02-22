c5c41ae1a2d71b7c656736ef132bf11b0a9dbbf8
al-ludwig <74364598+al-ludwig@users.noreply.github.com>
2021-02-22 17:26:06 +0100

    Modify multipliers

--- vivado/PowerMonitoring/PowerMonitoring.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ vivado/PowerMonitoring/PowerMonitoring.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
---Date        : Sat Feb 20 18:05:32 2021
+--Date        : Mon Feb 22 14:39:01 2021
 --Host        : DESKTOP-LG337B8 running 64-bit major release  (build 9200)
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
